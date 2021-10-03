d1a8b29cb0c5857ed15d65a62e3cc5b3064ca28b
Indrajith V <indrajithvgp@yahoo.com>
2021-10-03 11:47:42 +0530

    added meta tags and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Ed-Bucket</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/icons8-program-80.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
